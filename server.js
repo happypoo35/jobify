f1caf2777e36501d1eb54cb7a217c5af2c7003f4
Alex <happypoo35@gmail.com>
2022-06-04 11:49:20 +0300

    port

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
-      console.log(`Server is listening on ${port}...`);
+      console.log(`Server is listening on port ${port}...`);
     });
   } catch (err) {
     console.log(err);
